64c2ab18f47484cb76c2afa9a89965730ad02ef5
haoKahnguyen <haokah.nguyen@gmail.com>
2022-03-15 20:27:21 +0700

    update_loi_thieu_du_lieu

--- app.py
+++ app.py
@@ -15,36 +15,48 @@ def index():
 
 @app.route('/dauvao', methods=['POST'])
 def dauvao():
-    if request.method == 'POST':
-        gioitinh = request.form['gioitinh']
-        diemToan = request.form['diemToan']
-        diemVan = request.form['diemVan']
-        model = request.form['model']
-        sample_data = [gioitinh, diemToan, diemVan]
-        clean_data = [float(i) for i in sample_data]
-        ex1 = np.array(clean_data).reshape(1,-1)
-        if model == "svc-polynomial":
-            ic_model = joblib.load("data/svc_poly_model_df.pkl")
-        elif model == "svc-rbf":
-            ic_model = joblib.load("data/svc_rbf_model_df.pkl")
-        result_prediction = ic_model.predict(ex1)
+    try:
+        if request.method == 'POST':
+            gioitinh = request.form['gioitinh']
+            diemToan = request.form['diemToan']
+            diemVan = request.form['diemVan']
 
-        if result_prediction == [1]:
-            result_prediction = "Nhóm 1"
-        elif result_prediction == [2]:
-            result_prediction = "Nhóm 2"
-        elif result_prediction == [3]:
-            result_prediction = "Nhóm 3"
 
-        if gioitinh == "0":
-            gender = "Nam"
-        elif gioitinh == "1":
-            gender = "Nữ"
-    return render_template("index.html", gender=gender,
-        diemToan=diemToan,
-        diemVan=diemVan,
-        clean_data=clean_data,
-        result_prediction=result_prediction)
+            model = request.form['model']
+            normal_data = [gioitinh, diemToan, diemVan]
+            #convert
+            float_data = [float(i) for i in normal_data]
+            t = np.array(float_data).reshape(1,-1)
+            print(t)
+            if model == "svc-polynomial":
+                s_model = joblib.load("data/svc_poly_model_df.pkl")
+            elif model == "svc-rbf":
+                s_model = joblib.load("data/svc_rbf_model_df.pkl")
+            result_prediction = s_model.predict(t)
+
+            if result_prediction == [1]:
+                result_prediction = "Nhóm 1"
+            elif result_prediction == [2]:
+                result_prediction = "Nhóm 2"
+            elif result_prediction == [3]:
+                result_prediction = "Nhóm 3"
+            
+
+            if gioitinh == "0":
+                gender = "Nam"
+            elif gioitinh == "1":
+                gender = "Nữ"
+
+
+        return render_template("index.html", gender=gender,
+            diemToan=diemToan,
+            diemVan=diemVan,
+            result_prediction=result_prediction)
+    
+    except:
+        return render_template("index.html",
+            result_prediction="Chưa nhập đủ dữ liệu")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
